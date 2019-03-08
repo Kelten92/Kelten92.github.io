9f7b060538302d6437d0b83694581027dbadb40c
Kelten92 <mateuszsar@gmail.com>
2019-03-08 13:00:00 +0100

    navigation created

---
+++ precache-manifest.e63b87b0b384a9d69264a7de55b3f997.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d16f52ee52767f3b0dff",
+    "url": "/css/chunk-616d25e6.b9f2583c.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4d83eae3f566bc3539e2",
+    "url": "/js/chunk-vendors.465cea85.js"
+  },
+  {
+    "revision": "d16f52ee52767f3b0dff",
+    "url": "/js/chunk-616d25e6.46a9dbec.js"
+  },
+  {
+    "revision": "329d134d10c5d0ab0b40",
+    "url": "/js/chunk-2d0aed75.417a3dbf.js"
+  },
+  {
+    "revision": "d6bd8cd4579be5878ccf",
+    "url": "/js/chunk-2870351d.871138c9.js"
+  },
+  {
+    "revision": "28c3150e505bd6fd9608",
+    "url": "/js/app.b64ee4e6.js"
+  },
+  {
+    "revision": "33fe99458e2ca99570ac134f24596ad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87b467aa97b8fc387c99bb6c15bf2b08",
+    "url": "/img/knightMobile.87b467aa.jpg"
+  },
+  {
+    "revision": "d6bd8cd4579be5878ccf",
+    "url": "/css/chunk-2870351d.f1a96e04.css"
+  },
+  {
+    "revision": "28c3150e505bd6fd9608",
+    "url": "/css/app.8135f91b.css"
+  }
+];
